1d176757b417341f2e3125a692d9bf88619d8537
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-13 21:19:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,14 +3,12 @@ def countdown(num)
   num = 10
   while num > 0
     puts "#{num} SECOND(S)!"
-
+    countdown_with_sleep(5)
     num -= 1
-
   end
-  countdown_with_sleep(5)
+
     "HAPPY NEW YEAR!"
 end
-
 def countdown_with_sleep(num_sec)
   while num_sec > 0
     num_sec -= 1
